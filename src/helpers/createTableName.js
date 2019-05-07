1f1f06f18e535e7ebe57504c3a9368cef27dc904
Tomasz Gil <tomaszgil@hotmail.com>
2019-05-07 11:23:56 +0200

    Add checks for variable definition

--- src/helpers/createTableName.js
+++ src/helpers/createTableName.js
@@ -2,8 +2,10 @@ import { TRUNCATED_NAME_LENGTH } from '../core/dbConstants';
 import { truncateTableName } from '../../config.json';
 
 const createTableName = (name, path) => {
+  if (!path && !name) return '';
+
   let shortName = name;
-  if (truncateTableName) {
+  if (truncateTableName && name) {
     shortName = name.substring(0, TRUNCATED_NAME_LENGTH);
   }
 
