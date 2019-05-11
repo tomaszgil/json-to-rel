93523747f0579a1b79b0928b0eadefa29a2105c6
lukaszkobylecki <lukaszkobylecki@wp.pl>
2019-05-11 17:35:58 +0200

    code refactor

--- src/processing/preprocess.js
+++ src/processing/preprocess.js
@@ -2,12 +2,21 @@ import isObject from '../helpers/isObject';
 import Logger, { LogMessage } from '../helpers/Logger';
 import { generatedAttributeName } from '../../config.json';
 
-const handleSimpleTypes = (json) => {
+const preprocessing = (json) => {
   const processed = json;
 
   Object.keys(processed).forEach((key) => {
-    if (Array.isArray(processed[key]) || isObject(processed[key])) {
-      processed[key] = handleSimpleTypes(processed[key]);
+    if (Array.isArray(processed[key])) {
+      processed[key] = [].concat(...processed[key]);
+
+      if (processed[key].length === 0) {
+        delete processed[key];
+        return;
+      }
+
+      processed[key] = preprocessing(processed[key]);
+    } else if (isObject(processed[key])) {
+      processed[key] = preprocessing(processed[key]);
     } else if (Array.isArray(processed)) {
       processed[key] = {
         [generatedAttributeName]: processed[key],
@@ -18,49 +27,10 @@ const handleSimpleTypes = (json) => {
   return processed;
 };
 
-const flattenArrays = (json) => {
-  const processed = json;
-
-  Object.keys(processed).forEach((key) => {
-    if (Array.isArray(processed[key])) {
-      processed[key] = [].concat(...processed[key]);
-    }
-
-    if (Array.isArray(processed[key]) || isObject(processed[key])) {
-      processed[key] = flattenArrays(processed[key]);
-    }
-  });
-
-  return processed;
-};
-
-const deleteEmptyArrays = (json) => {
-  const processed = json;
-
-  Object.keys(processed).forEach((key) => {
-    if (Array.isArray(processed[key]) && processed[key].length === 0) {
-      delete processed[key];
-      return;
-    }
-
-    if (Array.isArray(processed[key]) || isObject(processed[key])) {
-      processed[key] = deleteEmptyArrays(processed[key]);
-    }
-  });
-
-  return processed;
-};
-
 const preprocess = (json) => {
-  let preprocessedJson = json;
-
   Logger.log(new LogMessage('Preprocessing json object.'));
 
-  preprocessedJson = handleSimpleTypes(preprocessedJson);
-  preprocessedJson = flattenArrays(preprocessedJson);
-  preprocessedJson = deleteEmptyArrays(preprocessedJson);
-
-  return preprocessedJson;
+  return preprocessing(json);
 };
 
 export default preprocess;
